c3f9209dfb93e4e1cc8c74bbab582824264e33f6
Rafael Finol <rafaelfinol88@gmail.com>
2023-03-23 00:36:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`)
+}
+// console.log(saturdayFun("Go to the movies"));
+
+function mondayWork (activity = "go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective (highlight = "*"){
+    return function (message = "special"){
+        return (`You are ${highlight}${message}${highlight}!`)       
+    }
+}
+//console.log(wrapAdjective("||")("a dedicated programmer"))
